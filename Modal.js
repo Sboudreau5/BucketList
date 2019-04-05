94be67c9c4b274be1b24c797ca5fb0e589705ffd
Shawn Boudreau <46534718+Sboudreau5@users.noreply.github.com>
2019-04-05 02:27:39 -0400

    Add files via upload

---
+++ Modal.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+
+document.getElementById('list-5').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal').style.display = 'flex'							   
+});
+
+
+document.querySelector('.close').addEventListener('click',
+	function() {
+	document.querySelector('.bg-modal').style.display = 'none'							   
+});
